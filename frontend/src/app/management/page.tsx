7468e2cf477466b166bf90f76eb4545ecd577767
ronanfitz <ronanfitz13@gmail.com>
2024-02-20 11:58:48 -0800

    Second-2-Implement navigation to requests and management pages

---
+++ frontend/src/app/management/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const ManagementPage: React.FC = () => {
+  return <div>Management</div>;
+};
+
+export default ManagementPage;
---
+++ frontend/src/app/reimbursement_requests/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const RequestsPage: React.FC = () => {
+  return <div>Requests</div>;
+};
+
+export default RequestsPage;
