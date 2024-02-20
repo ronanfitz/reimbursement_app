23c8a5b5931e17d6cfac1c5a90760d40f8ea3991
ronanfitz <ronanfitz13@gmail.com>
2024-02-20 11:23:28 -0800

    First-5-Implement endpoints for administrator get and post of requests

--- backend/endpoints/bank_administrator_endpoints.py
+++ backend/endpoints/bank_administrator_endpoints.py
@@ -22,12 +22,15 @@ def get_bank_administrators():
 
 @bank_administrator_blueprint.route('/<int:admin_id>/reimbursement_requests', methods=['GET'])
 def get_all_reimbursement_requests(admin_id):
-    # Placeholder Code
     with current_app.app_context():
         engine = create_engine(current_app.config['SQLALCHEMY_DATABASE_URI'])
         session = Session(bind=engine)
 
-        requests = session.query(BankAdministrator).all()
+        status = request.args.get('status', default='none')
+        if status != 'none':
+            requests = session.query(ReimbursementRequest).filter(ReimbursementRequest.status == status).all()
+        else:
+            requests = session.query(ReimbursementRequest).all()
 
         session.close()
 
@@ -36,14 +39,18 @@ def get_all_reimbursement_requests(admin_id):
 
 @bank_administrator_blueprint.route('/<int:admin_id>/reimbursement_requests/<int:request_id>', methods=['PUT'])
 def approve_or_deny_reimbursement_request(admin_id, request_id):
-    # Placeholder Code
     with current_app.app_context():
         engine = create_engine(current_app.config['SQLALCHEMY_DATABASE_URI'])
         session = Session(bind=engine)
 
-        requests = session.query(BankAdministrator).all()
+        content_type = request.headers.get('Content-Type')
+        if (content_type == 'application/json'):
+            requests = session.query(ReimbursementRequest).filter(ReimbursementRequest.id == request_id).first()
+            requests.status = request.json['status']
 
-        session.close()
+            session.commit()
+            session.close()
 
-        serialized_requests = [req.serialize() for req in requests]
-        return jsonify(serialized_requests)
+            return jsonify({'message': 'Reimbursement request updated successfully'})
+        else:
+            return 'Content-Type not supported!'
